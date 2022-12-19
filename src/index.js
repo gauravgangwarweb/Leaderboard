002edb4a463c0d074b944ea39af9d61636a75958
gauravgangwarweb <gauravgangwar501@gmail.com>
2022-12-19 21:22:02 +0530

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+const scoreList = `<p>Name: 100</p>
+<p>Name: 20</p>
+<p>Name: 40</p>
+<p>Name: 50</p>
+<p>Name: 60</p>`;
+
+document.querySelector('.list-block').innerHTML = scoreList;
