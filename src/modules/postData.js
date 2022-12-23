74739ba863145753024ebbe325bff35eb488e512
gauravgangwarweb <gauravgangwar501@gmail.com>
2022-12-23 12:21:57 +0530

    Modify src to fix some issues

--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,6 +1,8 @@
 const addNew = async () => {
   const inpName = document.querySelector('.inp-name');
   const inpScore = document.querySelector('.inp-score');
+  const emptyBLock = document.querySelector('.empty-data');
+  const alert = document.querySelector('.alert-block2');
   const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/YOk4bpeeBIDml5xqab0K/scores/';
 
   const playerData = {
@@ -16,8 +18,9 @@ const addNew = async () => {
     body: JSON.stringify(playerData),
   };
   await fetch(url, prost).then((response) => response.json())
-    .then(() => {
-      window.location.reload();
+    .then((data) => {
+      emptyBLock.innerHTML = `<p class="alert">${data.result}</p>`;
+      alert.innerHTML = '<p class="alert-2">Please click on refresh button to see the recent player data</p>';
     });
 };
 
