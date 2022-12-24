983d012d0269d296e6c2b17f75ed91a0c78de2f7
gauravgangwarweb <gauravgangwar501@gmail.com>
2022-12-24 18:10:03 +0530

    Modify src

--- src/modules/showData.js
+++ src/modules/showData.js
@@ -1,13 +1,17 @@
 const showData = (arr) => {
   const { result } = arr;
   const listBlock = document.querySelector('.list-block');
+  const totalPlyrs = document.querySelector('.total-plyrs');
+  let count = 0;
   if (!result.length) {
     listBlock.innerHTML = '<div class="empty-data">Nothing to show</div>';
   } else {
     result.forEach((x) => {
+      count++;
       listBlock.innerHTML += `<p class="data">${x.user}: ${x.score}</p>`;
     });
   }
+  totalPlyrs.innerHTML = `<p>Total Players: ${count}</p>`;
 };
 
 export default showData;
